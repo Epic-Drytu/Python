c81fe9b1f417cbc849f44500515c0bb012b0ce12
Epic-Drytu <harshilsrivastava09@gmail.com>
2026-02-27 03:22:31 +0530

    Update

--- Lecture_6.py
+++ Lecture_6.py
@@ -20,3 +20,32 @@ while abs(guess**2-x)>= epsilon:
 print("Number of guesses is equal to",num_guesses)
 print(guess,"is close to square root of",x)  
  
+ #To find the square root of 27
+cube=27
+epsilon=0.01
+low=0
+high=cube
+num_guesses=0
+guess=0
+while abs(guess**3-cube)>epsilon:
+    if guess**3>cube:
+        high=guess
+    else:
+        low=guess
+    guess=(high+low)/2
+    num_guesses+=1
+print("number of guesses made:",num_guesses)
+print(guess,"is close to cube root of",cube)
+
+# Newton-Raphson Root Finder
+#p(x)=x**2-24
+epsilon=0.01
+k=24.0
+guess=24.0/2
+num_guesses=0
+
+while abs(guess**2-k)>epsilon:
+    num_guesses+=1
+    guess=guess-(((guess**2)-k)/(2*guess))
+print("Number of guesses made is",num_guesses)
+print(guess,"is close to root of x**2-24")
