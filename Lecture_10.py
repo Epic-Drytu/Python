00c7b81cfc9404767921e65a10856233fc6b27f4
Epic-Drytu <harshilsrivastava09@gmail.com>
2026-03-15 23:55:24 +0530

    15 March

--- Lecture_10.py
+++ Lecture_10.py
@@ -12,3 +12,22 @@ print(L)
 
 #Output: ['re', ['do', ['re', 'mi']]], ['mi'], ['do', ['re', 'mi']], None
 
+#Write a function that meets the following specification:
+def make_ordered_list(n):
+    mylist=[]
+    for i in range(1, n+1):
+        mylist.append(i)
+    return mylist
+
+print(make_ordered_list(5))
+
+#Output: [1, 2, 3, 4, 5]
+
+def remove_element(L, e):
+    newlist=[]
+    for i in L:
+        if i not in e:
+            newlist.append(i)
+    return newlist  
+L=[1,2,3,4,5]
+print(remove_element(L, 3))
